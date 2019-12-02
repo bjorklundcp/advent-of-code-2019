c2aef73c4c0e3004e23ca0ce3f7e86c615126486
Cameron Bjorklund <cameron.bjorklund@singlewire.com>
2019-12-02 11:14:48 -0600

    day 1 and init commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject advent-of-code-2019 "0.1.0-SNAPSHOT"
+  :description "https://adventofcode.com/2019"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns advent-of-code-2019.core})
